cf88d03d30af20f7e540437ab82484265e218a05
Matt Holder <mjholder96@gmail.com>
2022-10-26 10:07:49 -0400

    initial commit

---
+++ metric_collector.py
@@ -0,0 +1,117 @@
+import re
+import requests
+import sys
+import os
+import urllib
+import json
+import datetime
+import time
+import psycopg2
+
+
+
+
+def main():
+    auth_token = os.environ.get('AUTH_TOKEN')
+
+    try:
+        connection = psycopg2.connect(  user=os.environ.get('DATABASE_USER'),
+                                        password = os.environ.get('DATABASE_PASSWORD'),
+                                        host = os.environ.get('POSTGRES_SQL_SERVICE_HOST'),
+                                        port = os.environ.get('POSTGRES_SQL_SERVICE_PORT'),
+                                        database = os.environ.get('DATABASE_NAME'))
+
+        connection.set_session(autocommit=True)
+
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        # Print PostgreSQL details
+        print("PostgreSQL server information")
+        # Executing a SQL query
+        cursor.execute("SELECT version();")
+        # Fetch result
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
+
+        create_tables(connection)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    
+
+    while(True):
+        SLO_dict = collect_SLO(auth_token)
+        process_SLO(SLO_dict, connection)
+
+        # run every 10 min
+        time.sleep(60)
+
+
+def create_tables(connection):
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS SLO")
+    create_table_query = '''CREATE TABLE SLO
+          (
+          SERVICE           TEXT    NOT NULL,
+          datetime          TIMESTAMP,
+          SLO_value         DOUBLE PRECISION); '''
+
+    cursor.execute(create_table_query)
+    connection.commit()
+
+def build_headers(auth_token):
+    print(auth_token)
+    headers = {
+        'authority': 'prometheus.crcs02ue1.devshift.net',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'en-US,en;q=0.9',
+        'cookie': auth_token,
+        'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    }
+
+    return headers
+
+
+def collect_SLO(auth_token):
+    url = 'https://prometheus.crcs02ue1.devshift.net/api/v1/query?query=sum(rate(api_3scale_gateway_api_status{status=%225xx%22}[8h]))/sum(rate(api_3scale_gateway_api_status[8h]))'
+    headers = build_headers(auth_token)
+    
+    # For some reason the request will fail if the query is passed in through 'params'
+    # url = "https://prometheus.crcs02ue1.devshift.net/api/v1/query"
+    # query = {"query": "sum(rate(api_3scale_gateway_api_status{status=%225xx%22}[8h]))/sum(rate(api_3scale_gateway_api_status[8h]))"}
+    # response = requests.get(url=url, params=query, headers=headers)
+    
+    response = requests.get(url=url, headers=headers)
+    response_json = response.json()
+    print(f"url = {response.url}\nresponse = {response_json}")
+
+    return {
+        'service': '3scale',
+        'datetime': datetime.datetime.now(),
+        'SLO': response_json['data']['result'][0]['value'][1]
+    }
+
+
+def process_SLO(SLO_dict, connection):
+    cursor = connection.cursor()
+
+    service_name = SLO_dict['service']
+    slo_datetime = SLO_dict['datetime']
+    slo_value = 1 - (float)(SLO_dict['SLO'])
+
+    print(service_name)
+    print(slo_datetime)
+    print(slo_value)
+
+    cursor.execute('insert into SLO values(%s,%s, %s)', (service_name, slo_datetime, slo_value))
+
+
+if __name__ == "__main__":
+    main()
